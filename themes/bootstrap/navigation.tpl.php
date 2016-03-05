034541ee35a13b2dbbc2dd53e15630896ae7bd48
Marc Hein <info@marc-hein.de>
2016-03-05 18:11:31 +0100

    Added first part of themes

---
+++ themes/bootstrap/navigation.tpl.php
@@ -0,0 +1,42 @@
+﻿<?php
+$is_login = $GLOBALS["is_login"];
+echo '
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            <a class="navbar-brand" href="'.url().'/">'.getPageName().'</a>
+          </div>
+          <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">';
+
+    $query = $GLOBALS["mysqli_connect"]->query("SELECT id, name FROM `pages` WHERE `id` <> 0 ORDER BY `pages`.`position` ASC");
+
+    while($result = mysqli_fetch_object($query)) {
+        echo "<li><a href='".url()."/?id=".$result->id."'>".$result->name."</a></li>";
+    }
+    $sql = "SELECT id FROM `pages` WHERE `name` LIKE 'Admin'";
+    $result = $GLOBALS["mysqli_connect"]->query($sql);
+    $adminid = (mysqli_fetch_object($result)->id);
+
+    echo'</ul><p class="navbar-btn">
+           <a href="'.url().'/?id='.$adminid.'" class="pull-right navbar-right btn btn-social btn-twitter"><i class="fa fa-lock"></i> Anmelden</a>
+           </p></div><!--/.nav-collapse -->
+        </div>
+      </nav>';
+
+    echo "\n";
+
+    if(getDebug()) {
+        if($is_login == 1) {
+            echo "<br />Eingeloggt.";
+        } else {
+            echo "<br />Nicht eingeloggt.";
+        }
+    }
+    ?>
