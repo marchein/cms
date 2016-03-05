5e23a4f9c5839533694dcbb84ad3d8c8f6fc152e
Marc Hein <info@marc-hein.de>
2016-03-05 23:02:17 +0100

    Added header.tpl.php for bootstrap

--- includes/templates/header.tpl.php
+++ includes/templates/header.tpl.php
@@ -6,9 +6,5 @@ echo "<!DOCTYPE html>
 <meta charset='utf-8'/>
 <title>" . getPageName() . "</title>\n";
 getLoggedIn();
-echo'
-    <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="css/style.css">
-</head>';
+echo'</head>';
 ?>
