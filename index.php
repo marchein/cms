99b7b9858e378065e888e7647407611060c1e801
Marc Hein <info@marc-hein.de>
2016-03-05 23:01:57 +0100

    Added advanced php error reporting for development

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ require_once ("includes/internal/admin_login.php");
 
 date_default_timezone_set('Europe/Berlin');
 mb_internal_encoding('UTF-8');
+ini_set ('display_errors',1);
+error_reporting (E_ALL & ~E_NOTICE); 
 
 getHeaders();
 
