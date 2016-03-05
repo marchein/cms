d5abccbfc952b57a03d0de627250faa9bc7c8b95
Marc Hein <info@marc-hein.de>
2016-03-06 00:54:51 +0100

    Added theme files

---
+++ includes/themes/bootstrap/theme.config.php
@@ -0,0 +1,8 @@
+﻿<?php
+$theme["name"] = "Bootstrap";
+$theme["version"] = "0.1 alpha";
+$theme["author"] = "Marc Hein";
+$theme["css"] = array("//netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css",
+                      "//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css",
+                      "https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css")
+?>
