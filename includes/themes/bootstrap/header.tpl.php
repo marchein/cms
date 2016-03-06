aa9693913f76ff1d7c22d56e97f86f00a597baa2
Marc Hein <info@marc-hein.de>
2016-03-06 10:58:12 +0100

    Added url parameter to theme.config.php for better management

--- includes/themes/bootstrap/header.tpl.php
+++ includes/themes/bootstrap/header.tpl.php
@@ -6,11 +6,11 @@ echo "<!DOCTYPE html>
 <meta charset='utf-8'/>
 <title>" . getPageName() . "</title>\n";
 getLoggedIn();
-foreach(glob("includes/themes/bootstrap/*.css") as $css) { // local css
+foreach(glob("includes/themes/".$theme["bootstrap"]["url"]."/*.css") as $css) { // local css
     echo '<link rel="stylesheet" href="'.$css.'">';
     echo "\n";
 }
-foreach($theme["bootstrap"]["css"] as $css) { // remote css
+foreach($theme[$theme["bootstrap"]["url"]]["css"] as $css) { // remote css
     echo '<link rel="stylesheet" href="'.$css.'">';
     echo "\n";
 }
