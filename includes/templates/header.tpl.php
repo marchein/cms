d2570d869198de4c191cc3b4b51aca48cf0e2dbb
Marc Hein <info@marc-hein.de>
2016-03-06 15:17:38 +0100

    Added ability to show exact page name and check if there are css files

--- includes/templates/header.tpl.php
+++ includes/templates/header.tpl.php
@@ -1,10 +1,19 @@
 ﻿<?php
 
-echo "<!DOCTYPE html>
+echo '<!DOCTYPE html>
 <html>
 <head>
-<meta charset='utf-8'/>
-<title>" . getPageName() . "</title>\n";
-getLoggedIn();
+<meta charset="utf-8"/>';
+echo '<title>'.getCurrentPageName($_GET["id"], $_GET["newsid"]).'</title>';
+foreach(glob("includes/themes/".$theme["default"]["url"]."/*.css") as $css) { // local css
+    echo '<link rel="stylesheet" href="'.$css.'">';
+    echo "\n";
+}
+if(!empty($theme[$theme["default"]["url"]]["css"])) {
+    foreach($theme[$theme["default"]["url"]]["css"] as $css) { // remote css
+        echo '<link rel="stylesheet" href="'.$css.'">';
+        echo "\n";
+    }
+}
 echo'</head>';
 ?>
