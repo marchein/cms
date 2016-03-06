e3083133f09ee8471623c9bc63164773e5536c0b
Marc Hein <info@marc-hein.de>
2016-03-06 13:07:31 +0100

    Removed login panel from Admincontent.php and included it from the current theme

--- includes/pages/Admincontent.php
+++ includes/pages/Admincontent.php
@@ -27,35 +27,11 @@ if ($is_login) {
     }
 } else {
     if(@$_GET["sp"]=="lost") {
-    include("plugins/forgot.php");
-} elseif(@$_GET["sp"]=="register") {
-    include("plugins/register.php");
-} else {
-    if(getLoginsend() == 0) {
-        echo '<form method="POST">
-        <input type="hidden" size="24" maxlength="50" name="id" value="0">
-        <input type="hidden" size="24" maxlength="50" name="log_in" value="1">
-        Dein User-Name:<br />
-        <input type="text" size="24" maxlength="50" name="login" value="">     <br />
-        Dein Passwort:<br />
-        <input type="password" size="24" maxlength="50" name="pw" value="">     <br />
-        <br />
-        <input type="submit" value="Anmelden">
-        </form>
-        <br /><br />
-        <a href="?id=0&amp;sp=lost">Passwort vergessen?</a> || <a href="?id=0
-        &amp;sp=register">Registrieren</a>';
+        include("plugins/forgot.php");
+    } elseif(@$_GET["sp"]=="register") {
+        include("plugins/register.php");
+    } else {
+        include ($GLOBALS["path"]."/login.tpl.php");
     }
-
-    if(getLoginsend() == 1) {
-        echo'Erfolgreich angemeldet! Klicke <a href="?id=0">hier</a> um fortzufahren.';
-    }
-    if(getLoginsend() == 2) {
-        echo'Falscher Benutzername und/oder falsches Passwort! Klicke <a href="?page=Login">hier</a> um\'s erneut zu versuchen.';
-    }
-    if(getLoginsend() == 3) {
-        echo'Erfolgreich abgemeldet! Klicke <a href="?page=Home">hier</a> um fortzufahren.';
-    }
-}
 }
 ?>
