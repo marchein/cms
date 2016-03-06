b4eb851a3b87a52de6e5488543bc853374a2ba4a
Marc Hein <info@marc-hein.de>
2016-03-06 13:05:34 +0100

    Added javascript to config

--- includes/themes/bootstrap/theme.config.php
+++ includes/themes/bootstrap/theme.config.php
@@ -1,9 +1,11 @@
 ﻿<?php
 $theme["bootstrap"]["name"] = "Bootstrap";
 $theme["bootstrap"]["url"] = "bootstrap";
-$theme["bootstrap"]["version"] = "0.1 alpha";
+$theme["bootstrap"]["version"] = "0.2 alpha";
 $theme["bootstrap"]["author"] = "Marc Hein";
 $theme["bootstrap"]["css"] = array("//netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css",
                       "//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css",
-                      "https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css");
+                      "//fortawesome.github.io/Font-Awesome/assets/font-awesome/css/font-awesome.css");
+$theme["bootstrap"]["js"] = array("//code.jquery.com/jquery.js",
+                                  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js");
 ?>
