5f04aeeb579887e3dee4c9f05c18e60d4f4182c8
Marc Hein <info@marc-hein.de>
2016-03-06 15:18:01 +0100

    Added ability to use js in footer.tpl.php

--- includes/templates/footer.tpl.php
+++ includes/templates/footer.tpl.php
@@ -1,4 +1,8 @@
 ﻿<?php
+foreach($theme[$theme["default"]["url"]]["js"] as $js) { // remote css
+    echo '<script src="'.$js.'"></script>';
+    echo "\n";
+}
 echo "</html>";
 echo "\n<!-- HeinCMS - Version: ".getVersion()." ".getVersionName()." -->";
 $mysqli_connect->close();
