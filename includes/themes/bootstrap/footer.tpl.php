32b6c75f3ee043f1933533290018c224cddf0721
Marc Hein <info@marc-hein.de>
2016-03-06 13:04:14 +0100

    Added js in footer.tpl.php

--- includes/themes/bootstrap/footer.tpl.php
+++ includes/themes/bootstrap/footer.tpl.php
@@ -1,4 +1,8 @@
 ﻿<?php
+foreach($theme[$theme["bootstrap"]["url"]]["js"] as $js) { // remote css
+    echo '<script src="'.$js.'"></script>';
+    echo "\n";
+}
 echo "</html>";
 echo "\n<!-- HeinCMS - Version: ".getVersion()." ".getVersionName()." -->";
 $mysqli_connect->close();
