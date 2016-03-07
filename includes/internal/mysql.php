248f374f569defee825bda500a3dcedb3cb83f96
Marc Hein <info@marc-hein.de>
2016-03-07 17:47:56 +0100

    Better error message

--- includes/internal/mysql.php
+++ includes/internal/mysql.php
@@ -1,7 +1,7 @@
 ﻿<?php
 @$mysqli = new mysqli($mysql_server,  $mysql_username,  $mysql_password, $mysql_database);
 if(isset($mysqli->connect_error)) {
-    die($mysqli->connect_error);
+    die("<h3>MySQL Fehler: ". $mysqli->connect_error ."</h3>\n<br/>Eventuell falschen Host in '/includes/internal/config.php' eingestellt? [In 99% der Fälle ist 'localhost' korrekt.]");
 } else {
     $GLOBALS["mysqli"] = $mysqli;
     $mysqli_connect = $mysqli;
