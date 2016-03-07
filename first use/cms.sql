17c7ef6b77c8b589ae61bbae5f97f906236c49b0
Marc Hein <info@marc-hein.de>
2016-03-07 15:32:17 +0100

    Updated cms.sql for current version (v0.1.0 Build: 4269)
    If you clone this, log in with user: dummy password: bla

--- first use/cms.sql
+++ first use/cms.sql
@@ -1,30 +1,11 @@
-﻿-- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Erstellungszeit: 04. Mrz 2016 um 16:31
--- Server Version: 5.6.21
--- PHP-Version: 5.6.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+﻿SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Datenbank: `cms`
---
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `config`
---
 
 CREATE TABLE IF NOT EXISTS `config` (
 `id` int(11) NOT NULL,
@@ -32,21 +13,12 @@ CREATE TABLE IF NOT EXISTS `config` (
   `mail` mediumtext,
   `debug` text NOT NULL,
   `version` varchar(10) NOT NULL,
-  `build` varchar(10) NOT NULL
+  `build` varchar(10) NOT NULL,
+  `theme` varchar(128) NOT NULL DEFAULT 'default'
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
---
--- Daten für Tabelle `config`
---
-
-INSERT INTO `config` (`id`, `name`, `mail`, `debug`, `version`, `build`) VALUES
-(1, 'CMS', 'user@server.com', '0', '0.0.5', '4198');
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `news`
---
+INSERT INTO `config` (`id`, `name`, `mail`, `debug`, `version`, `build`, `theme`) VALUES
+(1, 'HeinCMS', 'user@server.com', '0', '0.1.0', '4268', 'bootstrap');
 
 CREATE TABLE IF NOT EXISTS `news` (
 `id` int(11) NOT NULL,
@@ -56,35 +28,19 @@ CREATE TABLE IF NOT EXISTS `news` (
   `content` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `pages`
---
-
 CREATE TABLE IF NOT EXISTS `pages` (
 `id` int(11) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `position` varchar(3) NOT NULL,
-  `included` varchar(5) NOT NULL DEFAULT 'false',
+  `included` tinyint(1) NOT NULL DEFAULT '0',
   `content` text NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
---
--- Daten für Tabelle `pages`
---
-
 INSERT INTO `pages` (`id`, `name`, `date_created`, `position`, `included`, `content`) VALUES
-(0, 'Admin', '2015-06-19 10:46:11', '99', 'true', 'Interner Bereich'),
-(1, 'Home', '2015-06-18 06:41:52', '0', 'false', 'Home'),
-(2, 'News', '2015-06-20 15:59:52', '1', 'true', '');
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `user`
---
+(0, 'Admin', '2015-06-19 10:46:11', '99', 1, 'Interner Bereich'),
+(1, 'Home', '2015-06-18 06:41:52', '1', 0, '<p>Herzlich willkommen, beim neuen HeinCMS v2!<br /><br />Dieses CMS ist momentan im Aufbau und wird regelm&auml;&szlig;ig &uuml;berarbeitet.</p>\r\n<p>Mehr auf dem Git Repo unter: <a title="Github" href="https://github.com/marchein/cms">github.com/marchein/cms</a></p>'),
+(2, 'News', '2015-06-20 15:59:52', '2', 1, '');
 
 CREATE TABLE IF NOT EXISTS `user` (
 `ID` int(11) NOT NULL,
@@ -96,65 +52,31 @@ CREATE TABLE IF NOT EXISTS `user` (
   `length` varchar(128) DEFAULT NULL
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
---
--- Daten für Tabelle `user`
---
-
 INSERT INTO `user` (`ID`, `full_name`, `name`, `email`, `rechte`, `password`, `length`) VALUES
-(1, 'dummer user', 'dummy', 'user@server.com', '1', 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86', '3');
+(1, NULL, 'dummy', 'user@server.com', '1', '6ba028366ebcdcef1ce3d73883c3475def4b7925f80e800bc82d91aa4430093622f9f95d20894022864dfa55d9c901ff520e070132eafabdae62a73e5ceeaed1', '3');
 
---
--- Indizes der exportierten Tabellen
---
 
---
--- Indizes für die Tabelle `config`
---
 ALTER TABLE `config`
  ADD PRIMARY KEY (`id`);
 
---
--- Indizes für die Tabelle `news`
---
 ALTER TABLE `news`
  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
 
---
--- Indizes für die Tabelle `pages`
---
 ALTER TABLE `pages`
  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
 
---
--- Indizes für die Tabelle `user`
---
 ALTER TABLE `user`
  ADD PRIMARY KEY (`ID`), ADD UNIQUE KEY `ID` (`ID`);
 
---
--- AUTO_INCREMENT für exportierte Tabellen
---
 
---
--- AUTO_INCREMENT für Tabelle `config`
---
 ALTER TABLE `config`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT für Tabelle `news`
---
 ALTER TABLE `news`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT für Tabelle `pages`
---
 ALTER TABLE `pages`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT für Tabelle `user`
---
 ALTER TABLE `user`
-MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
