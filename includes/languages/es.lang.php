a6bbd4a6c458dde9c7e0eb9b27744271a5b34793
Marc Hein <info@marc-hein.de>
2016-04-02 22:44:53 +0200

    Added spanish language
    Thanks http://twitter.com/Aurorastrahl!

---
+++ includes/languages/es.lang.php
@@ -0,0 +1,72 @@
+﻿<?php
+
+/*
+----------------
+Language: Spanish
+----------------
+*/
+
+$language = array();
+
+$language['edit'] = "Editar algo";
+$language['menu'] = "Menú desplegable";
+$language['submit'] = "Mandar algo";
+$language['date'] = "el";
+
+// Rights
+$language['not_enough_rights'] = "Derecho de visita";
+$language['disabled'] = "Discapacitado";
+$language['administrator'] = "Administrador";
+$language['moderator'] = "Moderador";
+$language['normal_user'] = "Tuitero";
+
+// Intern
+$language['admin'] = "Administrador";
+$language['login'] = "Acceso";
+$language['logout'] = "Desconectarse";
+$language['username'] = "Usuario";
+$language['password'] = "Contraseña";
+$language['forgot_password'] = "¿Has olvidado tu contraseña?";   
+$language['please_login'] = "Por favor inicia sesión";
+$language['intern'] = "Interno";
+$language['loggedin'] = "¡Inicio de sesión exitoso! Clic <a href=\"?id=0\">aquí</a> para continuar.";
+$language['error_logging_in'] = "¡Error! Clic <a href=\"?id=0\">aquí</a> para volver a intentarlo.";
+$language['loggedout'] = "¡Cerrado la sesión con éxito! Clic <a href=\"?id=1\">aquí</a> para continuar.";
+$language['page_changed'] = "Página cambió";
+
+// Panelnames
+$language['config'] = "Configuraciones";
+$language['news'] = "Actualidades";
+$language['pages'] = "Páginas";
+$language['theme'] = "Diseño";
+$language['user'] = "Usuario";
+
+// Config
+$language['mysql_error'] = "¡MySQL error!";
+$language['mysql_error_help'] = "¡Error! anfitrión incorrecto '/includes/internal/config.php' ajustado? [En la mayoría de los casos 'localhost' correctamente.]";
+
+// Debug
+$language['cmsfolder'] = "El CMS se ejecuta en:";
+$language['cmsroot'] = "CMS raíz:";
+$language['cmsversion'] = "En la versión:";
+$language['cmspage'] = "Página:";
+
+// Menus
+$language['activated'] = "Activado";
+$language['deactivated'] = "Desactivado";
+$language['activate'] = "Habilitar?";
+$language['deactivate'] = "Desactivar?";
+
+// Pages
+$language['page_created'] = "Página creada con éxito<br />clic <a href=\"?id=0&amp;ap=Pages\">aquí</a> para continuar.";
+$language['page_title'] = "Título";
+$language['page_content'] = "Contenido de página";
+$language['page_include'] = "Lado /includes/pages/";
+$language['page_include_info'] = "(Si se selecciona este campo, el archivo debe estar en /includes/pages/ el nombre de archivo \"&#60;página del título&#62;content.php\" mentira. Además, el contenido en el campo \"contenido\" se ignora.)";
+$language['create_page'] = "Crear páginan";
+$language['delete_page'] = "Eliminar página existente";
+$language['order_page'] = "Cambia el orden de las páginas";
+
+// News
+$language['new_news'] = "Enviar un nuevo Noticias";
+?>
