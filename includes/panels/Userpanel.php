7be81ce40ed9c5f2a644f0f78b28999a0acbf7b9
Marc Hein <info@marc-hein.de>
2016-04-03 16:35:07 +0200

    Removed current implementation. New Userpanel.php soon

--- includes/panels/Userpanel.php
+++ includes/panels/Userpanel.php
@@ -3,46 +3,41 @@ $panelname = "User";
 $panelnames[$panelname] = $GLOBALS['lang']['user'];
 if(!isset($isinclude)) { $isinclude = true; }
 if($isinclude) {
-    if(isset($_GET["userlist"]) && $_GET["userlist"]) {
-        if(isset($_GET["userid"])) {
+    
+    /*
+    if(isset($_GET["userid"])) {
             $userid = $mysqli->real_escape_string($_GET["userid"]);
         }
-        if($userrights == 1) {
-            if(isset($userid)) {
-                $query = "SELECT * FROM `user` WHERE `id` = ".$userid;
-                $result = $mysqli->query($query);
-                $user = mysqli_fetch_object($result);
-                $fullname = $user->full_name;
-                if(!isset($fullname)) { $fullname = "Nicht angegeben"; }
-                echo "ID: " . $user->ID . "<br />
-                Username: " . $user->name . "<br />
-                Voller Name: " . $fullname . "<br />
-                E-Mail Adresse: " . $user->email . "<br />
-                Rechte: " . getRightsName($user->rights) . "<br />";
-            } else {
-                $query = "SELECT * FROM `user`";
-                $result = $mysqli->query($query);
-                // if($debug) { var_dump($result); }
-                echo "<table class='table'>
-                <tr>
-                    <th>Name</th>
-                    <th>Rechte</th>
-                </tr>";
-                while ($row = mysqli_fetch_object($result)) {
-                    $fullname = $row->full_name;
-                    if(!isset($fullname)) { $fullname = "Nicht angegeben"; }
-                    echo "<tr>
-                        <td><a href='?id=0&ap=User&userlist=true&amp;userid=".$row->ID."'>".$row->name."</a> (".$fullname.")</td>
-                        <td>".getRightsName($row->rights)."</td>
-                    </tr>";
-                }
-                echo "</table>";
-            }
+        if(isset($userid)) {
+            $query = "SELECT * FROM `user` WHERE `id` = ".$userid;
+            $result = $mysqli->query($query);
+            $user = mysqli_fetch_object($result);
+            $fullname = $user->full_name;
+            if(!isset($fullname)) { $fullname = "Nicht angegeben"; }
+            echo "ID: " . $user->ID . "<br />
+            Username: " . $user->name . "<br />
+            Voller Name: " . $fullname . "<br />
+            E-Mail Adresse: " . $user->email . "<br />
+            Rechte: " . getRightsName($user->rights) . "<br />";
         } else {
-            $showAccountSettings = true;
+            $query = "SELECT * FROM `user`";
+            $result = $mysqli->query($query);
+            // if($debug) { var_dump($result); }
+            echo "<table class='table'>
+            <tr>
+                <th>Name</th>
+                <th>Rechte</th>
+            </tr>";
+            while ($row = mysqli_fetch_object($result)) {
+                $fullname = $row->full_name;
+                if(!isset($fullname)) { $fullname = "Nicht angegeben"; }
+                echo "<tr>
+                    <td><a href='?id=0&ap=User&userlist=true&amp;userid=".$row->ID."'>".$row->name."</a> (".$fullname.")</td>
+                    <td>".getRightsName($row->rights)."</td>
+                </tr>";
+            }
+            echo "</table>";
         }
-    } else {
-        echo "<a href='?id=0&amp;ap=User&amp;userlist=true'>Nutzerliste</a><br />\n";
-    }
+        */
 }
 ?>
