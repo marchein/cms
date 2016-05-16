a0481c4e35bd339339d53f33f2e5bef921bf3157
Marc Hein <info@marc-hein.de>
2016-05-16 11:13:52 +0200

    [-] removed old GLOBALS variables

--- includes/internal/mysql.php
+++ includes/internal/mysql.php
@@ -4,8 +4,5 @@ if(isset($mysqli->connect_error)) {
     die("<h3>".$GLOBALS["lang"]["mysql_error"].": ". $mysqli->connect_error ."</h3>\n<br/>".$GLOBALS["lang"]["mysql_error_help"]);
 } else {
     $GLOBALS["mysqli"] = $mysqli;
-    $mysqli_connect = $mysqli;
-    $GLOBALS["mysqli_connect"] = $mysqli_connect;
-
 }
 ?>
